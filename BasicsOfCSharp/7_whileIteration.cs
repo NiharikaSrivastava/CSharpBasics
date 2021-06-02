83cf7f43f14519ff8c043b9a05fbad4151abf92b
unknown <niharika1420@gmail.com>
2021-06-02 21:06:51 +0530

    New Concept Hands On Added

---
+++ BasicsOfCSharp/7_whileIteration.cs
@@ -0,0 +1,76 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while(showMenu)  //display Menu and perform operations until we wish to exit
+            {
+                Console.Clear(); //to clear the console
+                showMenu=displayMenu();
+            }
+            Console.WriteLine("See you Later! Bye!");
+        }
+        private static bool displayMenu()
+        {
+            Console.Write("\n***** Game's Corner *****\n"
+                +"1. Calculate Factorial\n"
+                +"2. Guessing Game\n"
+                +"3. Exit\n"
+                +"Enter Your Choice: "
+                );
+            string choice = Console.ReadLine(); //As we read from console, always string returned
+            if (choice == "1")
+                calcFactorial();
+            else if (choice == "2")
+                guessingGame();
+            else if (choice == "3")
+                return false; // only when user selects exit
+            else
+                Console.WriteLine("Please Enter Choice as 1/2/3 Only.");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            return true;
+        }
+        private static void calcFactorial()
+        {
+            Console.Write("Calculating Factorial...\n"+"Enter Number to find its factorial: ");
+            int num = int.Parse(Console.ReadLine()); //Explicit Type Casting by using Parse Method
+            int counter=1, fact=1;
+            while (counter <= num)
+            {
+                fact = fact * counter;
+                counter++;
+            }
+            Console.WriteLine("Factorial of {0}: {1}",num,fact);
+        }
+        private static void guessingGame()
+        {
+            Console.WriteLine("Guessing Game...");
+            Random randObj = new Random(); //Random is a separate class in C Sharp
+            int randNumber = randObj.Next(1,11); //Next(start, end+1); as end is exclusive so rand number from 1 to 10 generated
+            int guesses = 0;
+            bool incorrect = true; 
+            string inputNum;
+            do
+            {
+                Console.Write("Enter your guess from 1 to 10: ");
+                inputNum = Console.ReadLine();
+                guesses++;
+                if(inputNum== randNumber.ToString())
+                {
+                    Console.WriteLine("Correct Guess!");
+                    incorrect = false;
+                }
+                Console.WriteLine("Wrong.. Keep Guessing!");
+            } while (incorrect);
+            Console.WriteLine("You guessed the correct number {0} in {1} Guesses!",randNumber,guesses);
+        }
+    }
+}
+*/
---
+++ BasicsOfCSharp/8_stringConcepts.cs
@@ -0,0 +1,82 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using escape sequences
+            string myStr = "\\This string uses \"escaped characters\"\\";
+            Console.WriteLine(myStr);
+            
+            //New Line Character
+            myStr = "Line 1\nLine 2";
+            Console.WriteLine(myStr);
+
+            //Using @ to directly display back-slash character
+            myStr = @"\This string uses escaped characters\";
+            Console.WriteLine(myStr);
+
+            //String.Format function with parameters (for replacement code)
+            myStr = String.Format("{1} and {0}","first","second"); //change of order of parameters, also use same parameter multiple times
+            Console.WriteLine(myStr);
+
+            //Refer documentation: https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+            //String.Format for Currency where :C -> C=Curency (My computer's language according to culture)
+            Console.WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
+            myStr = string.Format("{0:C}", 1200.25);
+            Console.WriteLine(myStr);
+
+            //String.Format for long numbers to add commas for readability
+            myStr = String.Format("{0:N}",123456789);
+            Console.WriteLine(myStr);
+
+            //String.Format for percentage
+            myStr = String.Format("{0:P}", 0.987); //multiplies by 100 and adds % sign 
+            Console.WriteLine(myStr);
+
+            //String.Format for phone numbers and formatting goes from right to left pound sign if extra numbers added
+            myStr = String.Format("{0:(+##) ##### #####}", 919876543210); //multiplies by 100 and adds % sign 
+            Console.WriteLine(myStr);
+
+            myStr = "   Let's practice C Sharp Language      ";
+            string mySubStr = myStr.Substring(5, 10); //get substring, start index, total chaacters 
+            Console.WriteLine(mySubStr);
+
+            myStr = myStr.ToUpper(); //convet to uppercase
+            Console.WriteLine(myStr);
+
+            myStr = myStr.ToLower(); //convet to lowercase
+            Console.WriteLine(myStr);
+
+            myStr = myStr.Replace(" ", "*"); //find every space i.e., " " and replace it with *
+            Console.WriteLine(myStr);
+            myStr = myStr.Replace("*", " ");
+
+            mySubStr = myStr.Remove(5, 10); //remove substring from string 
+            Console.WriteLine(mySubStr);
+
+            Console.WriteLine("Length of string originally: "+myStr.Length);
+            myStr = myStr.Trim(); //remove leading and trailing spaces
+            Console.WriteLine("String after removing leading and trailing spaces:" + myStr +"\nLength of string: "+myStr.Length);
+
+            //StringBuilder class used when heavy concatenation or string manipulation is required
+            StringBuilder concatStr=new StringBuilder();
+            int i = 1;
+            while(i<=20)
+            {
+                concatStr.Append(i);
+                concatStr.Append("---");
+                i++;
+            }
+            Console.WriteLine(concatStr);
+        }
+
+    }
+}
+*/
