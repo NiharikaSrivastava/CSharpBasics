b36a26ccadc59c5e808be1aba11e2f42303ddb10
unknown <niharika1420@gmail.com>
2021-06-05 22:33:34 +0530

    Moving to Main Branch

--- AsyncCSharp_RainerStropekCourse/AsyncCSharp_RainerStropekCourse/Program.cs
+++ AsyncCSharp_RainerStropekCourse/AsyncCSharp_RainerStropekCourse/Program.cs
@@ -6,7 +6,7 @@ namespace AsyncCSharp_RainerStropekCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
