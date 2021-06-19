873611994a57cf7212cc3e481dc17e71731dbed6
unknown <niharika1420@gmail.com>
2021-06-20 01:34:43 +0530

    Xamarin Practice

---
+++ XamarinBasics/StackLayout/StackLayout/StackLayout/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StackLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int clickCount = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            clickCount++;
+            ((Button)sender).Text =$"Clicked {clickCount} Times";
+        }
+    }
+}
