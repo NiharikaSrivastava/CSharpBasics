e9c739d54032274266e7a5bfc0f815ef654b4469
unknown <niharika1420@gmail.com>
2021-06-20 23:12:27 +0530

    Xamarin Practice

---
+++ XamarinBasics/TextTutorial/TextTutorial/TextTutorial/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TextTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue; //one letter back
+            string newText = e.NewTextValue; //current value
+            OldLabel.Text = oldText; 
+            NewLabel.Text = newText;
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string completedText = ((Entry)sender).Text; //after pressing enter
+            CompletedLabel.Text = completedText; 
+        }
+    }
+}
