08c26550a1b073158ccc356c5e281a0774bec2a6
unknown <niharika1420@gmail.com>
2021-07-05 04:42:16 +0530

    First Page Completed for AgentController App

---
+++ XamarinUI/AgentController/AgentController/AgentController/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+[assembly: ExportFont("Poppins-Light.ttf", Alias = "Poppins")]
+[assembly: ExportFont("Poppins-Medium.ttf", Alias = "Poppins-Medium")]
+
+namespace AgentController
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinUI/AgentController/AgentController/AgentController/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AgentController
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            loading.IsVisible = true;
+            Thread.Sleep(2000);
+            await Navigation.PushAsync(new ControllerRegistration());
+            loading.IsVisible = false;
+        }
+    }
+}
